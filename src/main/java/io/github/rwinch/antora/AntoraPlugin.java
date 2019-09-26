36ced6e8613f843499b913b27e886fb7eb044c51
Andreas Schilling <andreas_schilling@gmx.net>
2019-09-26 10:24:35 +0200

    Make plugin configurable and allow multiple antora tasks
    Using a separate AntoraTask it is now possible to hook in additional tasks. This allows
    to have multiple playbook files, e.g. for a local workflow.
    The plugin itself is now configurable, starting with the Antora version to be used.

--- src/main/java/io/github/rwinch/antora/AntoraPlugin.java
+++ src/main/java/io/github/rwinch/antora/AntoraPlugin.java
@@ -22,7 +22,6 @@ import com.moowork.gradle.node.npm.NpmTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
-import org.gradle.api.tasks.Exec;
 import org.gradle.internal.os.OperatingSystem;
 
 import java.io.File;
@@ -32,35 +31,85 @@ import java.util.Arrays;
  * @author Rob Winch
  */
 public class AntoraPlugin implements Plugin<Project> {
+
+	public static final String DOWNLOAD_ANTORA_TASK_NAME = "downloadAntora";
+
+	private static final String DOWNLOAD_ANTORA_CLI_TASK_NAME = "downloadAntoraCli";
+
+	private static final String DOWNLOAD_ANTORA_GENERATOR_TASK_NAME = "downloadAntoraSiteGenerator";
+
+	private static final String DEFAULT_ANTORA_TASK_NAME = "antora";
+
+	private static final String ANTORA_CLI_PACKAGE_NAME = "@antora/cli";
+
+	private static final String ANTORA_GENERATOR_PACKAGE_NAME = "@antora/site-generator-default";
+
+	private static final String INSTALL = "install";
+
+	private Project project;
+
 	@Override
 	public void apply(Project project) {
+		this.project = project;
+		project.getExtensions().create(Antora.ID, Antora.class);
 		project.getPlugins().apply(NodePlugin.class);
 
-		NodeExtension node = project.getExtensions().getByType(NodeExtension.class);
+		NodeExtension node = NodeExtension.get(project);
 		node.setDownload(true);
 		node.setVersion("8.11.4");
-		node.setNodeModulesDir(project.file(new File(project.getBuildDir(), "/modules")));
+		final File nodeModulesDir = project.file(new File(project.getBuildDir(), "/modules"));
+		node.setNodeModulesDir(nodeModulesDir);
 
+		registerSetupTasks();
+
+		registerDefaultTasks();
+
+		registerAntoraVersionOverrideHandler();
+	}
+
+	private void registerSetupTasks() {
 		NpmTask downloadAntoraCli = project.getTasks()
-				.create("downloadAntoraCli", NpmTask.class);
-		downloadAntoraCli.setArgs(Arrays.asList("install", "@antora/cli"));
+				.create(DOWNLOAD_ANTORA_CLI_TASK_NAME, NpmTask.class);
+		downloadAntoraCli.setArgs(Arrays.asList(INSTALL, ANTORA_CLI_PACKAGE_NAME));
 
 		NpmTask downloadAntoraSiteGenerator = project.getTasks()
-				.create("downloadAntoraSiteGenerator", NpmTask.class);
-		downloadAntoraSiteGenerator.setArgs(Arrays.asList("install", "@antora/site-generator-default"));
+				.create(DOWNLOAD_ANTORA_GENERATOR_TASK_NAME, NpmTask.class);
+		downloadAntoraSiteGenerator.setArgs(Arrays.asList(INSTALL, ANTORA_GENERATOR_PACKAGE_NAME));
 
-		Task downloadAntora = project.getTasks().create("downloadAntora");
-		downloadAntora.dependsOn(downloadAntoraCli, downloadAntoraSiteGenerator);
+		Task downloadAntora = project.getTasks()
+				.create(DOWNLOAD_ANTORA_TASK_NAME)
+				.dependsOn(downloadAntoraCli, downloadAntoraSiteGenerator);
+	}
 
-		Exec antora = project.getTasks().create("antora", Exec.class);
-		antora.setGroup("Docs");
-		antora.setDescription("Installs and runs antora site.yml");
-		antora.dependsOn(downloadAntora);
-		antora.setCommandLine(antoraCommand(), "site.yml");
+	private void registerDefaultTasks() {
+		project.getTasks().register(DEFAULT_ANTORA_TASK_NAME, AntoraTask.class);
+	}
+
+
+	private void registerAntoraVersionOverrideHandler() {
+		project.afterEvaluate(evaluatedProject -> {
+			if (config().getAntoraVersion().isPresent()) {
+				final String antoraVersion = config().getAntoraVersion().get();
+				project.getTasks()
+						.named(DOWNLOAD_ANTORA_CLI_TASK_NAME, NpmTask.class)
+						.configure(downloadAntoraCli -> {
+							downloadAntoraCli.setArgs(Arrays.asList(INSTALL, ANTORA_CLI_PACKAGE_NAME + "@" + antoraVersion));
+						});
+				project.getTasks()
+						.named(DOWNLOAD_ANTORA_GENERATOR_TASK_NAME, NpmTask.class)
+						.configure(downloadAntoraGenerator -> {
+							downloadAntoraGenerator.setArgs(Arrays.asList(INSTALL, ANTORA_GENERATOR_PACKAGE_NAME + "@" + antoraVersion));
+						});
+			}
+		});
 	}
 
 	private String antoraCommand() {
 		String command = "build/modules/node_modules/.bin/antora";
 		return OperatingSystem.current().isWindows() ? command + ".cmd" : command;
+	}
+
+	private Antora config() {
+		return (Antora) project.getExtensions().getByName(Antora.ID);
 	}
 }
